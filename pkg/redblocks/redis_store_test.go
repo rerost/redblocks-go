fe38d59b455a43f4f8f9a54a791190e07528fe87
rerost <gmailii@yahoo.co.jp>
2019-03-03 23:29:29 +0900

    Wrote order test

--- pkg/redblocks/redis_store_test.go
+++ pkg/redblocks/redis_store_test.go
@@ -345,3 +345,102 @@ func TestRedisStoreSubtraction(t *testing.T) {
 		t.Errorf(diff)
 	}
 }
+
+func TestRedisStoreIDsWithOrder(t *testing.T) {
+	redisStore := redblocks.NewRedisStore(&redis.Pool{
+		MaxIdle:     3,
+		IdleTimeout: 240 * time.Second,
+		Dial:        func() (redis.Conn, error) { return redis.Dial("tcp", "localhost:6379") },
+	})
+
+	key := "TestRedisStoreIDsWithOrder"
+	idsWithScore := []redblocks.IDWithScore{
+		{
+			ID:    "1",
+			Score: 1,
+		},
+		{
+			ID:    "2",
+			Score: 2,
+		},
+	}
+	ctx := context.Background()
+
+	err := redisStore.Save(ctx, key, idsWithScore, time.Second*100)
+	if err != nil {
+		t.Error(err)
+	}
+
+	ids, err := redisStore.GetIDs(ctx, key, 0, -1, redblocks.Asc)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if diff := cmp.Diff(ids, []redblocks.ID{"1", "2"}); diff != "" {
+		t.Error(diff)
+	}
+
+	ids, err = redisStore.GetIDs(ctx, key, 0, -1, redblocks.Desc)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if diff := cmp.Diff(ids, []redblocks.ID{"2", "1"}); diff != "" {
+		t.Error(diff)
+	}
+}
+
+func TestRedisStoreIDWithScoresWithOrder(t *testing.T) {
+	redisStore := redblocks.NewRedisStore(&redis.Pool{
+		MaxIdle:     3,
+		IdleTimeout: 240 * time.Second,
+		Dial:        func() (redis.Conn, error) { return redis.Dial("tcp", "localhost:6379") },
+	})
+
+	key := "TestRedisStoreIDWithScoresWithOrder"
+	idsWithScore := []redblocks.IDWithScore{
+		{
+			ID:    "1",
+			Score: 1,
+		},
+		{
+			ID:    "2",
+			Score: 2,
+		},
+	}
+
+	revIdsWithScore := []redblocks.IDWithScore{
+		{
+			ID:    "2",
+			Score: 2,
+		},
+		{
+			ID:    "1",
+			Score: 1,
+		},
+	}
+	ctx := context.Background()
+
+	err := redisStore.Save(ctx, key, idsWithScore, time.Second*100)
+	if err != nil {
+		t.Error(err)
+	}
+
+	result, err := redisStore.GetIDsWithScore(ctx, key, 0, -1, redblocks.Asc)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if diff := cmp.Diff(result, idsWithScore); diff != "" {
+		t.Error(diff)
+	}
+
+	result, err = redisStore.GetIDsWithScore(ctx, key, 0, -1, redblocks.Desc)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if diff := cmp.Diff(result, revIdsWithScore); diff != "" {
+		t.Error(diff)
+	}
+}
